7da5c9012671da2ff25c4daa2e416ba682d7f0a0
elipari1 <64856728+elipari1@users.noreply.github.com>
2020-08-07 18:00:28 -0400

    Add files via upload

---
+++ ERD Files/ERD Schema - final.sql
@@ -0,0 +1,23 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE "climate_delhi" (
+    "index" VARCHAR   NOT NULL,
+    "date" date   NOT NULL,
+    "meantemp" INT   NOT NULL,
+    "humidity" INT   NOT NULL,
+    "wind_speed" INT   NOT NULL,
+    "meanpressure" INT   NOT NULL
+);
+CREATE TABLE "air_quality_delhi" (
+    "index" VARCHAR   NOT NULL,
+    "date" date   NOT NULL,
+    "co" INT   NOT NULL,
+    "no2" INT   NOT NULL,
+    "o3" INT   NOT NULL,
+    "pm10" INT   NOT NULL,
+    "pm25" INT   NOT NULL,
+    "city" VARCHAR   NOT NULL
+);
+ALTER TABLE "climate_delhi" ADD CONSTRAINT "fk_climate_delhi_date" FOREIGN KEY("date")
+REFERENCES "air_quality_delhi" ("date");
